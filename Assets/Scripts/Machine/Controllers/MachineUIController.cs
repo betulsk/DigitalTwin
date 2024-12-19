8fa3e38ca68e1dbf82c41054d84408e6ce7d3a2c
Betul <betul.s.99@hotmail.com>
2024-12-19 16:21:35 +0300

    Machine UI update implementation

--- Assets/Scripts/Machine/Controllers/MachineUIController.cs
+++ Assets/Scripts/Machine/Controllers/MachineUIController.cs
@@ -1,27 +1,53 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MachineUIController : MonoBehaviour
 {
+    [Header("Controllers")]
     [SerializeField] private OEEController _oeeController;
+    [SerializeField] private OrderCodeController _orderCodeController;
+    [SerializeField] private MaterialCodeController _materialCodeController;
+
+    [Header("OEE Properties")]
     [SerializeField] private Image _oeeSliderImage;
-    [SerializeField] private TMP_Text _OEEText;
+    [SerializeField] private TMP_Text _oeeText;
+
+    [Header("Order Code Properties")]
+    [SerializeField] private TMP_Text _orderCodeText;
+
+    [Header("Material Code Properties")]
+    [SerializeField] private TMP_Text _materialCodeText;
 
     private void Awake()
     {
         _oeeController.OnOEEUpdated += OnOEEValueUpdated;
+        _orderCodeController.OnOrderCodeUpdated += OnOrderCodeUpdated;
+        _materialCodeController.OnMaterialCodeUpdated += OnMaterialCodeUpdated;
     }
 
     private void OnDestroy()
     {
         _oeeController.OnOEEUpdated -= OnOEEValueUpdated;
+        _orderCodeController.OnOrderCodeUpdated -= OnOrderCodeUpdated;
+        _materialCodeController.OnMaterialCodeUpdated -= OnMaterialCodeUpdated;
+    }
+
+    private void OnMaterialCodeUpdated()
+    {
+        _materialCodeText.SetText(ConstValues.MATERIAL + _materialCodeController.GetMaterialCode());
+    }
+
+    private void OnOrderCodeUpdated()
+    {
+        _orderCodeText.SetText(ConstValues.ORDER + _orderCodeController.GetOrderCode());
     }
 
     private void OnOEEValueUpdated()
     {
         _oeeSliderImage.fillAmount = _oeeController.GetCurrentOEE() /100f;
-        _OEEText.SetText(ConstValues.RATIO + _oeeController.GetCurrentOEE());
+        _oeeText.SetText(ConstValues.RATIO + _oeeController.GetCurrentOEE());
         Debug.Log("OEEValue is : " + _oeeController.GetCurrentOEE());
     }
 }
